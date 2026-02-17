92393d61155ad6d13d18eda337f25304c8a17ade
rajakiru <kiruthir@andrew.cmu.edu>
2026-02-17 08:49:57 -0500

    added web test pages for camera and viewer browser testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 import { setupSocket } from './socket';
 
@@ -15,6 +16,14 @@ const io = new Server(httpServer, {
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
+
+// Test pages
+app.get('/viewer', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'test-viewer.html'));
+});
+app.get('/camera', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'test-camera.html'));
+});
 
 // Health check
 app.get('/', (_req, res) => {
